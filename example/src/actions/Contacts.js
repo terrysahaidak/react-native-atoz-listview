9a80041062a0b87787fbff3a00ab54018d234ef3
Anh Tuan Nguyen <anhtuank7c@hotmail.com>
2017-02-26 08:15:37 +0700

    Sort result alphabet

--- example/src/actions/Contacts.js
+++ example/src/actions/Contacts.js
@@ -9,12 +9,19 @@ export const contactFetch = () => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(result => {
+                // group all contact by first letter in name field
                 const groups = _.groupBy(result, (user) => {
                     return user.name.charAt(0).toUpperCase();
                 });
+                // sort group
+                const keys = Object.keys(groups);
+                const sortedKeys = _.sortBy(keys);
+                const sortByKeys = _.fromPairs(
+                    _.map(sortedKeys, key => [key, groups[key]])
+                );
                 dispatch({
                     type: FETCH_CONTACT,
-                    data: groups,
+                    data: sortByKeys,
                     count: result.length
                 });
             })
